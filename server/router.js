f899f345ed0d0b22dcd823d78f98a7993e41172c
Rob Cornell <rob.cornell@gmail.com>
2017-07-08 11:17:44 -0400

    feat($api): Create placeholder getUserResults method

--- server/router.js
+++ server/router.js
@@ -4,4 +4,6 @@ const apiController = require('./apiController.js');
 router.get('/api/lightning', apiController.getTwoMovies);
 router.post('/api/lightning', apiController.chooseMovie);
 
+router.get('/api/results', apiController.getUserResults);
+
 module.exports = router;
